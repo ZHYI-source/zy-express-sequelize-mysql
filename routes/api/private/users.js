56bdb403ad9d4d1ebc0ef01930f9e390939e9010
Tom <1840354092@qq.com>
2022-04-05 16:47:27 +0800

    配置swagger

--- routes/api/private/users.js
+++ routes/api/private/users.js
@@ -3,38 +3,6 @@ const router = express.Router();
 const path = require("path");
 const logger = require("../../../utils/utils.logger").logger();
 
-/**,
- * @swagger
- * /list:
- *    get:
- *      tags:
- *      - 用户管理
- *      summary: 用户列表
- *      produces:
- *      - application/json
- *      parameters:
- *      - name: name
- *        in: query
- *        description: 姓名
- *        required: true
- *        type: string
- *        maximum:
- *        minimum: 1
- *        format:
- *      responses:
- *        200:
- *          description: successful operation
- *          schema:
- *            ref: #/definitions/Order
- *        400:
- *          description: Invalid ID supplied
- *        404:
- *          description: Order not found
- *          security:
-
- * */
-
-
 router.get('/list',
     // 验证参数֤
     (req, res, next) => {
