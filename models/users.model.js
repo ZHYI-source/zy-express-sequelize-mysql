3c9e8d543ad6d1f19e4c86e72fa2cc0a00ad4118
Tom <1840354092@qq.com>
2022-04-09 23:09:56 +0800

    swagger

--- models/users.model.js
+++ models/users.model.js
@@ -1,11 +1,19 @@
 //模型创建 https://www.sequelize.com.cn/core-concepts/model-basics#%E5%AD%97%E7%AC%A6%E4%B8%B2
+
+const {aes} = require('../utils/utils.crypto')
+
 module.exports = (sequelize, Sequelize) => {
     const Users = sequelize.define("users", {
         username: {
             type: Sequelize.STRING
         },
         password: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            set(value) {
+                // 在数据库中以明文形式存储密码是很糟糕的.
+                // 使用适当的aes对称加密更好.
+                this.setDataValue('password', aes.en(value));
+            }
         },
         nickName: {
             type: Sequelize.STRING,
